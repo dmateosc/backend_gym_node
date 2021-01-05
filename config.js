71aa147fd4d0117743ffd22dab3994654aa64e66
dmateosc <mateoscano.david@gmail.com>
2021-01-05 18:34:12 +0100

    make it better

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const MONGO_URL_GYM = process.env.MONGO_URL_GYM || "localhost:28017/gym";
 const MONGO_PPR = process.env.MONGO_PPR || "";
 const MONGO_USER = process.env.MONGO_USER  || "";
 const USER_PPR = process.env.NODE_ENV != 'production' ? `${MONGO_USER}${MONGO_PPR}`: `${MONGO_USER}:${MONGO_PPR}` 
-console.log(`${MONGO_BASE}${MONGO_USER}${MONGO_PPR}${MONGO_URL_GYM}`);
+console.log(`${MONGO_BASE}${USER_PPR}${MONGO_URL_GYM}`);
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   HOST: process.env.HOST || "127.0.0.1",
