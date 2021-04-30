83908bc2819f7d19e690774b2c1cc334c15e8406
dmateosc <mateoscano.david@gmail.com>
2021-04-30 14:32:01 +0200

    Subida de cambios

--- controllers/gym.js
+++ controllers/gym.js
@@ -7,213 +7,212 @@ var Musculo = require("../models/musculo");
 var Ejercicio = require("../models/ejercicio");
 var User = require("../models/usuario");
 
+//metodo para subir la imagen de un usuario
+const uploadUserImage = async (req, res) => {
+  var projectId = req.params.id;
+  var nickname = req.params.nickname;
 
-var GymController = {
-  uploadUserImage: function (req, res) {
-    var projectId = req.params.id;
-    var nickname = req.params.nickname;
+  var fileName = req.file;
+  var filePath = req.file.path;
+  var fileSplit = filePath.split("\\");
+  var fileName = fileSplit[1];
+  var extSplit = fileName.split(".");
+  var fileExt = extSplit[1];
 
-    var fileName = req.file;
-    var filePath = req.file.path;
-    var fileSplit = filePath.split("\\");
-    var fileName = fileSplit[1];
-    var extSplit = fileName.split(".");
-    var fileExt = extSplit[1];
-
-    if (nickname) {
-      User.updateOne(
+  if (nickname) {
+    try {
+      const imageUpdated = await User.updateOne(
         { nickname: nickname },
         {
           $set: { photo: fileName },
         },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El ejercicio no existe y no se ha podido añadir la imagen",
-              });
-
-          return res.status(200).send({
-            ejercicio: imageUpdated,
-          });
-        }
+        { new: true }
       );
-    } else if (projectId) {
-      Ejercicio.findByIdAndUpdate(
+      if (!imageUpdated)
+        return res.status(404).send({
+          message: "El ejercicio no existe y no se ha podido añadir la imagen",
+        });
+
+      return res.status(200).send({
+        ejercicio: imageUpdated,
+      });
+    } catch (error) {
+      return res.status(500).send("No se ha podido añadir la imagen");
+    }
+  } else if (projectId) {
+    try {
+      const imageUpdated = await Ejercicio.findByIdAndUpdate(
         projectId,
         {
           $set: { photo: fileName },
         },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El ejercicio no existe y no se ha podido añadir la imagen",
-              });
-
-          return res.status(200).send({
-            ejercicio: imageUpdated,
-          });
-        }
+        { new: true }
       );
+
+      if (!imageUpdated)
+        return res.status(404).send({
+          message: "El ejercicio no existe y no se ha podido añadir la imagen",
+        });
+
+      return res.status(200).send({
+        ejercicio: imageUpdated,
+      });
+    } catch (error) {
+      return res.status(500).send("No se ha podido añadir la imagen");
     }
-  },
+  }
 
-  uploadEjercicioImage: function (req, res) {
-    var projectId = req.params.id;
-    var nombreEjercicio = req.params.nombre;
+//subir la imagen en Base64 o bien subirla a una plataforma y de esa plataforma obtener la url
 
-    var fileName = req.file;
-    var filePath = req.file.path;
-    var fileSplit = filePath.split("\\");
-    var fileName = fileSplit[1];
-    var extSplit = fileName.split(".");
-    var fileExt = extSplit[1];
+};
 
-    if (nombreEjercicio) {
-      Ejercicio.updateOne(
+//Metodo para subir imagens de ejercicio //a futuro sera en base64 por comodidad
+const uploadEjercicioImage = async (req, res) => {
+  var projectId = req.params.id;
+  var nombreEjercicio = req.params.nombre;
+
+  var fileName = req.file;
+  var filePath = req.file.path;
+  var fileSplit = filePath.split("\\");
+  var fileName = fileSplit[1];
+  var extSplit = fileName.split(".");
+  var fileExt = extSplit[1];
+
+  //comprobamos si viene el nombre del ejercicio
+  if (nombreEjercicio) {
+    try {
+      const imageUpdated = await Ejercicio.updateOne(
         { nombre: nombreEjercicio },
         {
           $push: { imagenes: fileName },
         },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El ejercicio no existe y no se ha podido añadir la imagen",
-              });
-
-          return res.status(200).send({
-            ejercicio: imageUpdated,
-          });
-        }
+        { new: true }
       );
-    } else if (projectId) {
-      Ejercicio.findByIdAndUpdate(
+
+      if (!imageUpdated)
+        return res.status(404).send({
+          message: "El ejercicio no existe y no se ha podido añadir la imagen",
+        });
+
+      return res.status(200).send({
+        ejercicio: imageUpdated,
+      });
+    } catch (error) {
+      return res.status(500).send("No se ha podido añadir la imagen");
+    }
+
+    //comprobamos el projectId para actualizar
+  } else if (projectId) {
+    try {
+      const imageUpdated = await Ejercicio.findByIdAndUpdate(
         projectId,
         {
           $push: { imagenes: fileName },
         },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El ejercicio no existe y no se ha podido añadir la imagen",
-              });
-
-          return res.status(200).send({
-            ejercicio: imageUpdated,
-          });
-        }
+        { new: true }
       );
-    }
-  },
-  uploadMusculoImage: function (req, res) {
-    var projectId = req.params.id;
-    var nombreMusculo = req.params.nombre;
 
-    var fileName = req.file;
-    var filePath = req.file.path;
-    var fileSplit = filePath.split("\\");
-    var fileName = fileSplit[2];
-    var extSplit = fileName.split(".");
-    var fileExt = extSplit[1];
-    if (nombreMusculo) {
-      Musculo.updateOne(
-        { nombre: nombreMusculo },
-        {
-          $push: { imagenes: fileName },
-        },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El músculo no existe y no se ha podido añadir la imagen",
-              });
+      if (!imageUpdated)
+        return res.status(404).send({
+          message: "El ejercicio no existe y no se ha podido añadir la imagen",
+        });
 
-          return res.status(200).send({
-            musculol: imageUpdated,
-          });
-        }
-      );
-    } else if (projectId) {
-      Musculo.findByIdAndUpdate(
-        projectId,
-        {
-          $push: { imagenes: fileName },
-        },
-        { new: true },
-        (err, imageUpdated) => {
-          if (err)
-            return res.status(500).send("No se ha podido añadir la imagen");
-          if (!imageUpdated)
-            return res
-              .status(404)
-              .send({
-                message:
-                  "El músculo no existe y no se ha podido añadir la imagen",
-              });
-
-          return res.status(200).send({
-            musculo: imageUpdated,
-          });
-        }
-      );
+      return res.status(200).send({
+        ejercicio: imageUpdated,
+      });
+    } catch (error) {
+      return res.status(500).send("No se ha podido añadir la imagen");
     }
-  },
-  getImage: function(req,res){
-    var file = req.params.image;
-    var type = req.params.type;
-    if (type == 'muscles') {
-      var path_file = './uploads/muscles/'+file;
-    }
-    else if(type == 'exercise'){
-      var path_file = './uploads/exercise/'+file;
-    }else if(type == 'user'){
-      var path_file = './uploads/user/'+file;
-    }else{
-      return res.status(400).send("No hay un tipo definido");
-    }
-		
-		
-		//se usa exists aunque este deprecated
-		fs.exists(path_file, (exists) => {
-			if(exists){
-				return res.sendFile(path.resolve(path_file));
-			}else{
-				return res.status(200).send({
-					message: "No existe la imagen..."
-				});
-			}
-		});
-
   }
-  
 };
 
-module.exports = GymController;
+
+//metodo para actualizar la imagen del musculo
+const uploadMusculoImage = async (req, res) => {
+
+  
+  var projectId = req.params.id;
+  var nombreMusculo = req.params.nombre;
+
+  var fileName = req.file;
+  var filePath = req.file.path;
+  var fileSplit = filePath.split("\\");
+  var fileName = fileSplit[2];
+  var extSplit = fileName.split(".");
+  var fileExt = extSplit[1];
+  if (nombreMusculo) {
+    Musculo.updateOne(
+      { nombre: nombreMusculo },
+      {
+        $push: { imagenes: fileName },
+      },
+      { new: true },
+      (err, imageUpdated) => {
+        if (err)
+          return res.status(500).send("No se ha podido añadir la imagen");
+        if (!imageUpdated)
+          return res.status(404).send({
+            message: "El músculo no existe y no se ha podido añadir la imagen",
+          });
+
+        return res.status(200).send({
+          musculol: imageUpdated,
+        });
+      }
+    );
+  } else if (projectId) {
+    Musculo.findByIdAndUpdate(
+      projectId,
+      {
+        $push: { imagenes: fileName },
+      },
+      { new: true },
+      (err, imageUpdated) => {
+        if (err)
+          return res.status(500).send("No se ha podido añadir la imagen");
+        if (!imageUpdated)
+          return res.status(404).send({
+            message: "El músculo no existe y no se ha podido añadir la imagen",
+          });
+
+        return res.status(200).send({
+          musculo: imageUpdated,
+        });
+      }
+    );
+  }
+};
+
+
+const getImage = (req, res) => {
+  var file = req.params.image;
+  var type = req.params.type;
+  if (type == "muscles") {
+    var path_file = "./uploads/muscles/" + file;
+  } else if (type == "exercise") {
+    var path_file = "./uploads/exercise/" + file;
+  } else if (type == "user") {
+    var path_file = "./uploads/user/" + file;
+  } else {
+    return res.status(400).send("No hay un tipo definido");
+  }
+
+  //se usa exists aunque este deprecated
+  fs.exists(path_file, (exists) => {
+    if (exists) {
+      return res.sendFile(path.resolve(path_file));
+    } else {
+      return res.status(200).send({
+        message: "No existe la imagen...",
+      });
+    }
+  });
+};
+
+
+module.exports = {
+  uploadUserImage,
+  uploadEjercicioImage,
+  uploadMusculoImage,
+  getImage,
+}
